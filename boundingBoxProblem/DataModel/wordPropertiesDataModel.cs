e6ef3a9413c3209017449a8c5ecca1a2ca94b9f0
Abdullah Maher <Abdullah.Maher@linkdev.com>
2024-12-15 17:50:39 +0200

    FirstCommit

--- boundingBoxProblem/DataModel/wordPropertiesDataModel.cs
+++ boundingBoxProblem/DataModel/wordPropertiesDataModel.cs
@@ -4,8 +4,8 @@
     {
         public int x { get; set; }
         public int y { get; set; }
-        public int width { get; set; }
-        public int height { get; set; }
+        public double width { get; set; }
+        public double height { get; set; }
     }
 
 
